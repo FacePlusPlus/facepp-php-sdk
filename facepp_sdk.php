d9b73cba8b0af8fb5df8a4154b42431f26540d8d
tytymnty <tytymnty@163.com>
2014-02-28 10:58:25 +0800

    update

--- facepp_sdk.php
+++ facepp_sdk.php
@@ -80,38 +80,3 @@ class Facepp{
         return array('http_code'=>$reponse_header['http_code'],'request_url'=>$request_url,'body'=>$response_text);
     }
 }
-
-########################
-###     example      ###
-########################
-$facepp = new Facepp();
-
-#detect local image 
-$params=array('img'=>'{image file path}');
-$params['attribute'] = 'gender,age,race,smiling,glass,pose';
-$response = $facepp->execute('/detection/detect',$params);
-print_r($response);
-
-#detect image by url
-$params=array('url'=>'http://www.faceplusplus.com.cn/wp-content/themes/faceplusplus/assets/img/demo/1.jpg');
-$response = $facepp->execute('/detection/detect',$params);
-print_r($response);
-/*
-if($response['http_code']==200){
-    #json decode 
-    $data = json_decode($response['body'],1);
-    #get face landmark
-    foreach ($data['face'] as $face) {
-        $response = $facepp->execute('/detection/landmark',array('face_id'=>$face['face_id']));
-        print_r($response);
-    }
-    #create person 
-    $response = $facepp->execute('/person/create',array('person_name'=>'unique_person_name'));
-    print_r($response);
-
-    #delete person
-    $response = $facepp->execute('/person/delete',array('person_name'=>'unique_person_name'));
-    print_r($response);
-
-}
-*/
